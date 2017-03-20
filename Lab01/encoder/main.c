12aac5b2c59e38a62935b92e85f01f79002f29b5
Amit Marx <amitm@wix.com>
2017-03-21 01:22:08 +0200

    third stage

--- Lab01/encoder/main.c
+++ Lab01/encoder/main.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
+int getOffset(int argc, char **argv){
+    int offset = 0;
+    char* enc = NULL;
+    int j;
+    for(j=0;j<argc;j++){
+        if(argv[j][0]=='-'||argv[j][0]=='+')
+            enc = argv[j];
+    }
+    if(enc!=NULL){
+        int i=1;
+            while(enc[i]!='\0'){
+               offset*=10;
+               offset+=enc[i]-'0';
+               i++;
+            }
+            if(enc[0]=='-'){
+                offset*=-1;
+            }
+        }
+    return offset;
+}
+
+FILE* getStream(int argc, char **argv){
+    int i;
+    char* path = NULL; 
+    for(i=0;i<argc;i++){
+        if(strcmp(argv[i],"-i")==0)
+            path = argv[i+1];
+    }
+    FILE* stream;
+    if(path!=NULL)
+        stream = fopen(path,"r");
+    else
+        stream = stdin;
+    return stream;
+}
 
 int main(int argc, char **argv)
 {
+    FILE* stream =NULL;
+    stream = getStream(argc,argv);
     int letter = 0;
     int offset = getOffset(argc,argv);
     while(letter != EOF){
-       letter = fgetc(stdin);
+       letter = fgetc(stream);
        if(letter != EOF){
             if ((letter >= 65) && (letter <= 90))
                 letter = letter + 32;
@@ -19,20 +57,4 @@ int main(int argc, char **argv)
         fputc(letter,stdout);
        }
     }
-}
-int getOffset(int argc, char **argv){
-    int offset = 0;
-    if(argc==2){
-    char* enc = argv[1]; 
-    int i=1;
-        while(enc[i]!='\0'){
-           offset*=10;
-           offset+=enc[i]-'0';
-           i++;
-        }
-        if(enc[0]=='-'){
-            offset*=-1;
-        }
-    }
-    return offset;
 }
