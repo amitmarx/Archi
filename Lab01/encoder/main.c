e7141035451e50d723f5bec789b83169a2045335
Amit Marx <amitm@wix.com>
2017-03-21 00:21:15 +0200

    second stage fixed

--- Lab01/encoder/main.c
+++ Lab01/encoder/main.c
@@ -3,6 +3,24 @@
 int main(int argc, char **argv)
 {
     int letter = 0;
+    int offset = getOffset(argc,argv);
+    while(letter != EOF){
+       letter = fgetc(stdin);
+       if(letter != EOF){
+            if ((letter >= 65) && (letter <= 90))
+                letter = letter + 32;
+            if(letter >= 97 && letter<= 122){
+                letter+=offset;
+                if(letter<97)
+                    letter= 123 - (97-letter);
+                if(letter>122)
+                    letter = 97 + (123-letter);
+            }
+        fputc(letter,stdout);
+       }
+    }
+}
+int getOffset(int argc, char **argv){
     int offset = 0;
     if(argc==2){
     char* enc = argv[1]; 
@@ -16,20 +34,5 @@ int main(int argc, char **argv)
             offset*=-1;
         }
     }
-    while(letter != EOF){
-       letter = fgetc(stdin);
-       if(letter != EOF){
-            if ((letter >= 65) && (letter <= 90))
-                letter = letter + 32;
-            if(letter >= 97 && letter<= 122){
-            letter+=offset;
-            letter=(letter%123);
-            if(letter<65){
-                letter += 97;
-            }
-            }
-        fputc(letter,stdout);
-       }
-    }
+    return offset;
 }
-
