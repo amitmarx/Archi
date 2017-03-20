2442f3d4e5b814ad80029d02ef4776c0da6fe1a1
Amit Marx <amitm@wix.com>
2017-03-20 21:57:33 +0200

    second stage

--- Lab01/encoder/main.c
+++ Lab01/encoder/main.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
     int letter = 0;
-    
+    int offset = 0;
+    if(argc==2){
+    char* enc = argv[1]; 
+    int i=1;
+        while(enc[i]!='\0'){
+           offset*=10;
+           offset+=enc[i]-'0';
+           i++;
+        }
+        if(enc[0]=='-'){
+            offset*=-1;
+        }
+    }
     while(letter != EOF){
        letter = fgetc(stdin);
        if(letter != EOF){
             if ((letter >= 65) && (letter <= 90))
-                letter = letter + 32; 
+                letter = letter + 32;
+            if(letter >= 97 && letter<= 122){
+            letter+=offset;
+            letter=(letter%123);
+            if(letter<65){
+                letter += 97;
+            }
+            }
         fputc(letter,stdout);
        }
     }
 }
+
