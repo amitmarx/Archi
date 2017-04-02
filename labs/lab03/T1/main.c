0f0f86428f401c7b5462b0579ac11dd420e9806d
Amit Marx <amitm@wix.com>
2017-04-03 01:09:14 +0300

    1a done

--- labs/lab03/T1/main.c
+++ labs/lab03/T1/main.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 
 typedef struct virus virus;
-virus * initVirus(unsigned short length, char* name);
+virus *initVirus(unsigned short length, char *name);
 void PrintHex(char *buffer, int length);
-void PrintVirus(virus * v);
+void PrintVirus(virus *v);
 struct virus
 {
     unsigned short length;
@@ -19,45 +19,57 @@ int main(int argc, char **argv)
     char encoding[2];
     fread(encoding, 1, 2, f);
     bool isBigEndian;
-    isBigEndian = encoding[0] == '1';
-    int index=0;
-    virus * v;
+    isBigEndian = (unsigned char)encoding[0] == '\x01';
+    virus *v;
     while (1)
     {
         char len[2];
         if (fread(len, 1, 2, f) != 2)
             break;
-        
-        unsigned short length = (int)len[0] * 16 + (int)len[1] - 18;;
-        v= malloc(sizeof(virus)+length*sizeof(char));
+
+        unsigned short length;
+        if (isBigEndian)
+        {
+            length = (int)len[0] * 16 + (int)len[1] - 18;
+        }
+        else
+        {
+            length = (int)len[1] * 16 + (int)len[0] - 18;
+        }
+        v = malloc(sizeof(virus) + length * sizeof(char));
         v->length = length;
         fread(v->name, 1, length + 16, f);
         PrintVirus(v);
         free(v);
-        index++;
     }
     fclose(f);
 }
-void PrintVirus(virus * v){
-    printf("Virus name: %s\nVirus size: %d\nsignature:\n",v->name,v->length);
-    PrintHex(v->signature,v->length);
+void PrintVirus(virus *v)
+{
+    printf("Virus name: %s\nVirus size: %d\nsignature:\n", v->name, v->length);
+    PrintHex(v->signature, v->length);
+    printf("\n");
 }
-virus * initVirus(unsigned short length, char* name){
-    virus * v= malloc(sizeof(virus)+length*sizeof(char));
+virus *initVirus(unsigned short length, char *name)
+{
+    virus *v = malloc(sizeof(virus) + length * sizeof(char));
     v->length = length;
     int i;
-    for(i=0;i<16;i++){
-        (v->name)[i] = *(name+i);
+    for (i = 0; i < 16; i++)
+    {
+        (v->name)[i] = *(name + i);
     }
     return v;
 }
 
-void PrintHex(char * buffer, int length)
+void PrintHex(char *buffer, int length)
 {
     int i;
-    for (i = 0; i < length; i++){
-        printf("%02hhX ", (unsigned char)(*(buffer+i)));
-        if ((i+1) % 20 == 0){
+    for (i = 0; i < length; i++)
+    {
+        printf("%02hhX ", (unsigned char)(*(buffer + i)));
+        if ((i + 1) % 20 == 0)
+        {
             printf("\n");
         }
     }
