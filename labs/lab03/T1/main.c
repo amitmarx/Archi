bb29cdd9a436ad0540d0c7f83671b30b5be0fea6
Amit Marx <amitm@wix.com>
2017-04-02 22:02:50 +0300

    working as requested

--- labs/lab03/T1/main.c
+++ labs/lab03/T1/main.c
@@ -16,12 +16,12 @@ struct virus
 int main(int argc, char **argv)
 {
     FILE *f = fopen("signatures", "r");
-    virus * viruses [200];
     char encoding[2];
     fread(encoding, 1, 2, f);
     bool isBigEndian;
     isBigEndian = encoding[0] == '1';
     int index=0;
+    virus * v;
     while (1)
     {
         char len[2];
@@ -29,21 +29,14 @@ int main(int argc, char **argv)
             break;
         
         unsigned short length = (int)len[0] * 16 + (int)len[1] - 18;;
-        char name [16];
-        
-        fread(name, 1, 16, f);
-        
-        /*char * signature = ( char *)malloc(length);
-        fread(signature, 1, length, f);*/
-
-        viruses[index] = initVirus(length,name);
-        int x = fread(viruses[index]->signature, 1, length, f);
+        v= malloc(sizeof(virus)+length*sizeof(char));
+        v->length = length;
+        fread(v->name, 1, length + 16, f);
+        PrintVirus(v);
+        free(v);
         index++;
     }
-    for(int i=0; i< index; i++){
-        PrintVirus(viruses[i]);
-        printf("\n");
-    }
+    fclose(f);
 }
 void PrintVirus(virus * v){
     printf("Virus name: %s\nVirus size: %d\nsignature:\n",v->name,v->length);
